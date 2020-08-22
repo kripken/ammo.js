260456a4f81baae9e26f72b7aaf4c2b921c4d3c2
Ian Purvis <ian.c.purvis@gmail.com>
2020-08-22 16:59:06 -0500

    Provide some addFunction wasm signature constants

--- onload.js
+++ onload.js
@@ -1,2 +1,7 @@
+Module['CONTACT_ADDED_CALLBACK_SIGNATURE'] = 'iiiiiiii';
+Module['CONTACT_DESTROYED_CALLBACK_SIGNATURE'] = 'ii';
+Module['CONTACT_PROCESSED_CALLBACK_SIGNATURE'] = 'iiii';
+Module['INTERNAL_TICK_CALLBACK_SIGNATURE'] = 'vif';
+
 // Reassign global Ammo to the loaded module:
 this['Ammo'] = Module;
