f471cd15ae51474891c34cd295ef75588bcb7434
Ian Purvis <ian.c.purvis@gmail.com>
2020-08-23 11:23:50 -0500

    Adds test coverage for all contact callbacks

--- tests/add-function.js
+++ tests/add-function.js
@@ -1,9 +1,9 @@
 const test = require('ava')
 const createUnitBox = require('./helpers/create-unit-box.js')
+const createDiscreteDynamicsWorld = require('./helpers/create-discrete-dynamics-world.js')
 const loadAmmo = require('./helpers/load-ammo.js')
 
 const CF_CUSTOM_MATERIAL_CALLBACK = 8
-
 
 function shouldTest(t, Ammo) {
   if (Ammo.addFunction) return true
@@ -12,48 +12,36 @@ function shouldTest(t, Ammo) {
   return false
 }
 
+// Test contact callbacks serially:
+// Each btManifoldResult callback is a global extern in bullet, however ammo
+// has made these available via instance method on btDiscreteDynamicsWorld
+// Since all instances share the same global, concurrent testing is unsafe.
+const testContactCallback = test.serial.cb
+
 // Load global Ammo once for all tests:
 test.before(async t => await loadAmmo())
 
-test('Ammo.addFunction should return a function pointer', t => {
+
+testContactCallback('btDiscreteDynamicsWorld.prototype.setContactAddedCallback should work like a charm', t => {
 
   if (!shouldTest(t, Ammo)) return
 
-  const functi0n = () => true
-  const pointer = Ammo.addFunction(functi0n)
-  t.assert(pointer)
-})
-
-
-test('btDiscreteDynamicsWorld.prototype.setContactAddedCallback should work like a charm', t => {
-
-  if (!shouldTest(t, Ammo)) return
-
-  const collisionConfiguration = new Ammo.btDefaultCollisionConfiguration()
-  const dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration)
-  const broadphase = new Ammo.btDbvtBroadphase()
-  const solver = new Ammo.btSequentialImpulseConstraintSolver()
-  const dynamicsWorld = new Ammo.btDiscreteDynamicsWorld(
-    dispatcher,
-    broadphase,
-    solver,
-    collisionConfiguration
-  )
+  const world = createDiscreteDynamicsWorld()
+  const timestep = 1
+  const maxSubsteps = 0
 
   // Place two boxes at the origin to generate a collision:
   const bodyA = createUnitBox()
   bodyA.setCollisionFlags(CF_CUSTOM_MATERIAL_CALLBACK)
-  dynamicsWorld.addRigidBody(bodyA)
+  world.addRigidBody(bodyA)
 
   const bodyB = createUnitBox()
   bodyB.setCollisionFlags(CF_CUSTOM_MATERIAL_CALLBACK)
-  dynamicsWorld.addRigidBody(bodyB)
+  world.addRigidBody(bodyB)
 
-  // Set up the callback:
-  let callbackInvocationCount = 0
+  // Set up the callback
+  let expectedInvocations = 4
   const callback = (cp, colObj0Wrap, partId0, index0, colObj1Wrap, partId1, index1) => {
-    callbackInvocationCount++
-
     t.not(cp, undefined)
     t.not(colObj0Wrap, undefined)
     t.not(partId0, undefined)
@@ -69,45 +57,131 @@ test('btDiscreteDynamicsWorld.prototype.setContactAddedCallback should work like
     colObj1Wrap = Ammo.wrapPointer(colObj1Wrap, Ammo.btCollisionObjectWrapper)
     colObj1 = colObj1Wrap.getCollisionObject()
     t.assert(Ammo.compare(colObj1, bodyB))
+
+    if (--expectedInvocations < 1) t.end()
   }
-  const callbackPointer = Ammo.addFunction(callback)
-  dynamicsWorld.setContactAddedCallback(callbackPointer)
+  callback.pointer = Ammo.addFunction(callback, Ammo.CONTACT_ADDED_CALLBACK_SIGNATURE)
+  world.setContactAddedCallback(callback.pointer)
+  t.teardown(() => world.setContactAddedCallback(null))
 
   // Step the simulation and assert the behavior:
-  dynamicsWorld.stepSimulation(1)
-  t.is(callbackInvocationCount, 4)
+  world.stepSimulation(timestep, maxSubsteps)
 })
 
 
-test('btDynamicsWorld.prototype.setInternalTickCallback should work like a charm', t => {
+testContactCallback('btDiscreteDynamicsWorld.prototype.setContactDestroyedCallback should work like a charm', t => {
 
   if (!shouldTest(t, Ammo)) return
 
-  const collisionConfiguration = new Ammo.btDefaultCollisionConfiguration()
-  const dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration)
-  const broadphase = new Ammo.btDbvtBroadphase()
-  const solver = new Ammo.btSequentialImpulseConstraintSolver()
-  const dynamicsWorld = new Ammo.btDiscreteDynamicsWorld(
-    dispatcher,
-    broadphase,
-    solver,
-    collisionConfiguration
-  )
-  const timestep = 1 // integer is easy to assert
-  const maxSubSteps = 0 // required for a stable callback timestep
+  const world = createDiscreteDynamicsWorld()
+  const timestep = 1
+  const maxSubsteps = 0
+
+  // Place two boxes at the origin to generate a collision
+  const bodyA = createUnitBox()
+  bodyA.setCollisionFlags(CF_CUSTOM_MATERIAL_CALLBACK)
+  world.addRigidBody(bodyA)
+
+  const bodyB = createUnitBox()
+  bodyB.setCollisionFlags(CF_CUSTOM_MATERIAL_CALLBACK)
+  world.addRigidBody(bodyB)
 
   // Set up the callback:
-  let callbackInvocationCount = 0
-  const callback = (_world, _timestep) => {
-    callbackInvocationCount++
-    _world = Ammo.wrapPointer(_world, Ammo.btDiscreteDynamicsWorld)
-    t.assert(Ammo.compare(_world, dynamicsWorld))
-    t.is(_timestep, timestep)
+  const expectedUserPersistentData = []
+  let expectedInvocations = 4
+  const callback = (userPersistentData) => {
+    t.assert(expectedUserPersistentData.includes(userPersistentData))
+    if (--expectedInvocations < 1) t.end()
   }
-  const callbackPointer = Ammo.addFunction(callback)
-  dynamicsWorld.setInternalTickCallback(callbackPointer)
+  callback.pointer = Ammo.addFunction(callback, Ammo.CONTACT_DESTROYED_CALLBACK_SIGNATURE)
+  world.setContactDestroyedCallback(callback.pointer)
+  t.teardown(() => world.setContactDestroyedCallback(null))
+
+  // Step the simulation to start the collision:
+  world.stepSimulation(timestep, maxSubsteps)
+
+  // Assign user persistent data to trigger the destroy callback:
+  // In bullet, contact manifold user persistent data can be any void*
+  // ammo does not have a way to create pointers to javascript data,
+  // but you can abuse the void* to store a simple int value.
+  // Here we create some random integers for testing the callback:
+  const dispatcher = world.getDispatcher()
+  for (let i = 0, manifold; i < dispatcher.getNumManifolds(); i++) {
+    manifold = dispatcher.getManifoldByIndexInternal(i)
+    for (let j = 0, point; j < manifold.getNumContacts(); j++) {
+      point = manifold.getContactPoint(j)
+      point.m_userPersistentData = Math.floor(Math.random() * 100)
+      expectedUserPersistentData.push(point.m_userPersistentData)
+    }
+  }
+
+  // Separate the boxes to end the collision:
+  bodyA.getWorldTransform().getOrigin().setValue(10, 10, 10)
 
   // Step the simulation and assert the behavior:
-  dynamicsWorld.stepSimulation(timestep, maxSubSteps)
-  t.is(callbackInvocationCount, 1)
+  world.stepSimulation(timestep, maxSubsteps)
+})
+
+
+testContactCallback('btDiscreteDynamicsWorld.prototype.setContactProcessedCallback should work like a charm', t => {
+
+  if (!shouldTest(t, Ammo)) return
+
+  const world = createDiscreteDynamicsWorld()
+  const timestep = 1
+  const maxSubsteps = 0
+
+  // Place two boxes at the origin to generate a collision:
+  const bodyA = createUnitBox()
+  bodyA.setCollisionFlags(CF_CUSTOM_MATERIAL_CALLBACK)
+  world.addRigidBody(bodyA)
+
+  const bodyB = createUnitBox()
+  bodyB.setCollisionFlags(CF_CUSTOM_MATERIAL_CALLBACK)
+  world.addRigidBody(bodyB)
+
+  // Set up the callback:
+  let expectedInvocations = 4
+  const callback = (cp, body0, body1) => {
+    t.not(cp, undefined)
+
+    body0 = Ammo.wrapPointer(body0, Ammo.btCollisionObject)
+    t.assert(Ammo.compare(body0, bodyA))
+
+    body1 = Ammo.wrapPointer(body1, Ammo.btCollisionObject)
+    t.assert(Ammo.compare(body1, bodyB))
+
+    if (--expectedInvocations < 1) t.end()
+  }
+  const callbackPointer = Ammo.addFunction(callback, Ammo.CONTACT_PROCESSED_CALLBACK_SIGNATURE)
+  world.setContactProcessedCallback(callbackPointer)
+  t.teardown(() => world.setContactProcessedCallback(null))
+
+  // Step the simulation and assert the behavior:
+  world.stepSimulation(timestep, maxSubsteps)
+})
+
+
+test.cb('btDynamicsWorld.prototype.setInternalTickCallback should work like a charm', t => {
+
+  if (!shouldTest(t, Ammo)) return
+
+  const world = createDiscreteDynamicsWorld()
+  const timestep = 1 // integer is easy to assert
+  const maxSubsteps = 0 // required for a stable callback timestep
+
+  // Set up the callback:
+  let expectedInvocations = 1
+  const callback = (_world, _timestep) => {
+    _world = Ammo.wrapPointer(_world, Ammo.btDiscreteDynamicsWorld)
+    t.assert(Ammo.compare(_world, world))
+    t.is(_timestep, timestep)
+    if (--expectedInvocations < 1) t.end()
+  }
+  callback.pointer = Ammo.addFunction(callback, Ammo.INTERNAL_TICK_CALLBACK_SIGNATURE)
+  world.setInternalTickCallback(callback.pointer)
+  t.teardown(() => world.setInternalTickCallback(null))
+
+  // Step the simulation and assert the behavior:
+  world.stepSimulation(timestep, maxSubsteps)
 })
